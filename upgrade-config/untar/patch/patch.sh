728b07ed38ab8045abb5efaadf20cc316e4ffccd
Christian Mauderer <oss@c-mauderer.de>
2019-12-12 16:53:32 +0100

    Fix path collision in patch and sysupgrade.
    My patch.sh used /tmp/root for mounting the root file system. That
    collided with RAM_ROOT from the sysupgrade scripts (or rather
    /lib/upgrade/common.sh). Therefore the manipulated updater couldn't be
    added to the ramfs. Changing that path solved everything.

--- upgrade-config/untar/patch/patch.sh
+++ upgrade-config/untar/patch/patch.sh
@@ -10,16 +10,12 @@ set -x
 # - Copy and patch fwupdate.real (which is a link to ubntbox) to skip the
 #   signature check. This is used by the patched /lib/upgrade scripts.
 
-LIB_UPGRADE_PATCHED="/tmp/libupgrade"
 UBNTBOX_PATCHED="/tmp/fwupdate.real"
-UBNTBOX_SEEKOFFSET=24598
 
 MD5FILE="/tmp/patchmd5"
 
 cat <<EOF > ${MD5FILE}
-54cb4e1ff4567a868f220960715938db  /sbin/sysupgrade
 c33235322da5baca5a7b237c09bc8df1  /sbin/fwupdate.real
-6fc82bd8a79cc02ec684de593db2901f  /lib/upgrade/platform.sh
 EOF
 
 # check md5 of files that will be patched
@@ -30,12 +26,9 @@ then
 fi
 
 # prepare some overlay functionality
-LOWERDIR="/tmp/root"
-if [ ! -e ${LOWERDIR} ]
-then
-	mkdir -p ${LOWERDIR}
-	mount -t squashfs -oro /dev/mtdblock3 ${LOWERDIR}
-fi
+LOWERDIR="/tmp/lower_root"
+mkdir -p ${LOWERDIR}
+mount -t squashfs -oro /dev/mtdblock3 ${LOWERDIR}
 overlay_some_path()
 {
 	PATH_TO_OVERLAY=$1
@@ -49,16 +42,8 @@ overlay_some_path()
 	mount -t overlay -o lowerdir=${LOWERDIR}${PATH_TO_OVERLAY},upperdir=${UPPERDIR},workdir=${WORKDIR} overlay ${PATH_TO_OVERLAY}
 }
 
-# Patch sysupgrade in place
+# patch the ubntbox binary.
 overlay_some_path "/sbin" "sbin"
-sed -i "s|include /lib/upgrade|include ${LIB_UPGRADE_PATCHED}|" /sbin/sysupgrade
-
-# Copy and patch the ubntbox binary.
-cp /sbin/fwupdate.real ${UBNTBOX_PATCHED}
-echo -en '\x10' | dd of=${UBNTBOX_PATCHED} conv=notrunc bs=1 count=1 seek=24598
-
-# Copy and patch the scripts
-cp -r /lib/upgrade ${LIB_UPGRADE_PATCHED}
-sed -i "s|echo \"/sbin/fwupdate.real\"|echo \"${UBNTBOX_PATCHED}\"|g" ${LIB_UPGRADE_PATCHED}/platform.sh
+echo -en '\x10' | dd of=/sbin/fwupdate.real conv=notrunc bs=1 count=1 seek=24598
 
 echo "******** Done ********"
